2648cd2c535def4d623e0b66b3b417f39af19241
Batuhan Güneş <35640085+bba2han@users.noreply.github.com>
2023-10-04 18:44:18 +0300

    UI upgrades

--- app.js
+++ app.js
@@ -12,24 +12,26 @@ addToDoButton.addEventListener("click", addToDo);
 
 
 function addToDo(){
+    const textBoxValue = toDo.value.trim();
     if(toDo.value == null || toDo.value == ""){
         alertMessage("danger", "Please write a to do!");
     }
     else{
-    addToDoToUI();
+    addToDoToUI(textBoxValue);
     alertMessage("success", "You added to do successfully!");
     }
 }
 
-function addToDoToUI(){
+function addToDoToUI(v){
     const li = document.createElement("li");
     li.className = "list-group-item";
-    li.textContent = toDo.value + " ";
+    li.textContent = v;
 
     const a = document.createElement("a");
     a.className = "delete-item";
-    a.textContent = "delete";
+    a.textContent = "Delete";
     a.href = "#";
+    a.style.padding = "10px";
 
     li.appendChild(a);
     toDoList.appendChild(li);
@@ -37,13 +39,16 @@ function addToDoToUI(){
 
 function alertMessage(type, message){
     const divalert = document.createElement("div");
+    const hr = document.createElement("hr");
     divalert.className = `alert alert-${type}`;
     divalert.role = "alert";
     divalert.textContent = `${message}`;
 
+    cardBody1.appendChild(hr);
     cardBody1.appendChild(divalert);
 
     setTimeout(() => {
+        hr.remove();
         divalert.remove();
     }, 2500);
 }
